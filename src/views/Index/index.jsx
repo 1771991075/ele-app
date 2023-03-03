a837e3fb8462801699891b29164b95964cf8d910
lzc <lvzicong123@outlook.com>
2023-03-03 20:52:12 +0800

    修复导航栏显示异常

--- src/views/Index/index.jsx
+++ src/views/Index/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { Tabbar } from 'react-vant';
 import { HomeO, GuideO, OrdersO, UserO } from '@react-vant/icons';
 import './index.less'
 
 export default function Index() {
+    let [active, setActive] = useState(0)
     let navigate = useNavigate()
+    let location = useLocation()
+    let path = location.pathname;
+
+    useEffect(() => {
+        switch (path) {
+            case '/index/home':
+                setActive(0)
+                break
+            case '/index/search':
+                setActive(1)
+                break
+            case '/index/order':
+                setActive(2)
+                break
+            case '/index/mine':
+                setActive(3)
+                break
+            default:
+                setActive(0)
+                break
+        }
+    }, [path])
 
     let goPage = (active) => {
         switch (active) {
@@ -24,7 +47,7 @@ export default function Index() {
     }
     return (
         <div className='index1'>
-            <Tabbar onChange={(active) => goPage(active)}>
+            <Tabbar onChange={(active) => goPage(active)} value={active}>
                 <Tabbar.Item icon={<HomeO />}>外卖</Tabbar.Item>
                 <Tabbar.Item icon={<GuideO />}>搜索</Tabbar.Item>
                 <Tabbar.Item icon={<OrdersO />}>订单</Tabbar.Item>
--- src/views/Mine/index.jsx
+++ src/views/Mine/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { NavBar} from 'react-vant';
+import { PhoneO ,Arrow} from '@react-vant/icons';
+import './index.less'
 
 export default function Mine() {
+  let navigate = useNavigate()
   return (
-    <div>Mine</div>
+    <div className='mine'>
+        <NavBar
+        className='mine_nav'
+        title="我的"
+        onClickLeft={() => navigate(-1)}
+      />
+      <div className='mineInfo'>
+        <div className='mineimg'>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <span>登录</span>|<span>注册</span>
+          <PhoneO  />
+        </div>
+        <div className='mineright'>
+          <Arrow fontSize={20}/>
+        </div>
+      </div>
+    </div>
   )
 }
