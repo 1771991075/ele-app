ac0da3d288677fd108566fb136c96a28d6013171
lzc <lvzicong123@outlook.com>
2023-03-03 20:13:29 +0800

    新增搜索页面

--- src/views/SearchInfo/index.jsx
+++ src/views/SearchInfo/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { NavBar, Button } from 'react-vant';
+import './index.less'
 
 export default function SearchInfo() {
+  let navigate = useNavigate()
+
   return (
-    <div>Search</div>
+    <div className='searchInfo'>
+      <NavBar
+        className='searchInfo_nav'
+        title="搜索"
+        onClickLeft={() => navigate(-1)}
+      />
+      <div className='searchInfobtn'>
+        <Button type='info'>拍照</Button>
+        <Button type='info'>相册</Button>
+        <Button type='info'>预览图片</Button>
+        <Button type='info'>系统分享</Button>
+      </div>
+    </div>
   )
 }
