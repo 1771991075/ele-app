313d33cae6e0fa540b3461a5fd297d1a379c49aa
lzc <lvzicong123@outlook.com>
2023-03-04 09:10:59 +0800

    修改排版

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useSearchParams } from "react-router-dom"
 import { getHeohash, getHomeCate, getShop } from '../../api/home'
-import { getNowCity} from '../../api/index'
+import { getNowCity } from '../../api/index'
 import { Search, ShopO } from '@react-vant/icons';
 import { Rate } from 'react-vant';
 import HomeSwiper from "../../component/Home/Swiper";
@@ -19,11 +19,11 @@ export default function Home() {
 
     useEffect(() => {
         //获取地区详细信息
-        if(!geohash){
-            getNowCity().then(res=>{
+        if (!geohash) {
+            getNowCity().then(res => {
                 getHeohash(res.data.geohash).then(res => setCityInfo(res.data))
             })
-        }else{
+        } else {
             getHeohash(geohash).then(res => setCityInfo(res.data))
         }
         //获取home分类列表
@@ -31,7 +31,7 @@ export default function Home() {
         //获取附近商家
         getShop(cityInfo.latitude, cityInfo.longitude).then(res => setShopList(res.data))
 
-    }, [geohash,cityInfo.latitude, cityInfo.longitude])
+    }, [geohash, cityInfo.latitude, cityInfo.longitude])
 
     return (
         <div className="home">
@@ -39,7 +39,7 @@ export default function Home() {
                 <div className="home_search">
                     <Search fontSize={25} />
                 </div>
-                <div className="home_title" onClick={()=>navigate('/selectCity')}>
+                <div className="home_title" onClick={() => navigate('/selectCity')}>
                     <p>{cityInfo.name}</p>
                 </div>
                 <div>
@@ -58,15 +58,19 @@ export default function Home() {
                                 <div className="shopitemleft">
                                     <img src={`https://elm.cangdu.org/img/${item.image_path}`} alt="" />
                                 </div>
-                                <div className="shopitemmid">
-                                    <div className="mid_top"><span className="pinpai">品牌</span><span className="mid_name">{item.name}</span></div>
-                                    <div className="mid_mid"><Rate value={item.rating} size={12} gutter={1} color={'rgb(255, 210, 30)'} /><span className="star"> {item.rating}</span><span className="yuexiao"> 月销{item.recent_order_num}单</span></div>
-                                    <div className="mid_btm">￥{item.float_minimum_order_amount}起送/{item.piecewise_agent_fee.tips}</div>
-                                </div>
                                 <div className="shopitemright">
-                                    <div className="right_top"><span className="bzp">保</span><span className="bzp">准</span><span className="bzp">票</span></div>
-                                    <div className="right_mid"><span className="fnzs">{item.delivery_mode.text}</span><span className="zsd">准时达</span></div>
-                                    <div className="right_btm">{item.distance} / <span className="pssj">{item.order_lead_time}</span></div>
+                                    <div className="item_top">
+                                        <div className="mid_top"><span className="pinpai">品牌</span><span className="mid_name">{item.name}</span></div>
+                                        <div className="right_top"><span className="bzp">保</span><span className="bzp">准</span><span className="bzp">票</span></div>
+                                    </div>
+                                    <div className="item_mid">
+                                        <div className="mid_mid"><Rate value={item.rating} size={12} gutter={1} color={'rgb(255, 210, 30)'} /><span className="star"> {item.rating}</span><span className="yuexiao"> 月销{item.recent_order_num}单</span></div>
+                                        <div className="right_mid"><span className="fnzs">{item.delivery_mode.text}</span><span className="zsd">准时达</span></div>
+                                    </div>
+                                    <div className="item_btm">
+                                        <div className="mid_btm">￥{item.float_minimum_order_amount}起送/{item.piecewise_agent_fee.tips}</div>
+                                        <div className="right_btm">{item.distance} / <span className="pssj">{item.order_lead_time}</span></div>
+                                    </div>
                                 </div>
                             </div>
                         )
